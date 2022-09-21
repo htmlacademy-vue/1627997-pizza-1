c0eba3bf625732a32530fb85ee41a137e87f4caf
woodward42 <andrey.ivanov.5891@gmail.com>
2022-09-21 14:46:48 +0300

    Первое задание готово

---
+++ src/frontend/src/common/constants.js
@@ -0,0 +1,29 @@
+/* eslint-disable prettier/prettier */
+export const PIZZA_SIZES = {
+  "23 см": "small",
+  "32 см": "normal",
+  "45 см": "big",
+};
+
+export const INGREDIENTS_ENG_NAMES = {
+  "Грибы": "mushrooms",
+  "Чеддер": "cheddar",
+  "Салями": "salami",
+  "Ветчина": "ham",
+  "Ананас": "ananas",
+  "Бекон": "bacon",
+  "Лук": "onion",
+  "Чили": "chile",
+  "Халапеньо": "jalapeno",
+  "Маслины": "olives",
+  "Томаты": "tomatoes",
+  "Лосось": "salmon",
+  "Моцарелла": "mozzarella",
+  "Пармезан": "parmesan",
+  "Блю чиз": "blue_cheese"
+};
+
+export const SAUCES_ENG_NAMES = {
+  "Томатный": "tomato",
+  "Сливочный": "creamy",
+};
