dc309a78b249f7b073384aeaeef2a36cd632ab35
woodward42 <andrey.ivanov.5891@gmail.com>
2022-10-26 14:24:26 +0300

    Сделал рефакторинг Index, добавил лейауты и рабочий роутер

---
+++ src/frontend/src/layouts/AppLayoutHeader.vue
@@ -0,0 +1,66 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <header class="header">
+    <div class="header__logo">
+      <router-link to="/" class="logo">
+        <img
+          src="@/assets/img/logo.svg"
+          alt="V!U!E! Pizza logo"
+          width="90"
+          height="40"
+        />
+      </router-link>
+    </div>
+    <div class="header__cart">
+      <router-link to="/cart"> 0 ₽ </router-link>
+    </div>
+    <div class="header__user">
+      <router-link 
+        v-if="!isAuthorized" 
+        to="/login" 
+        class="header__login"
+      >
+        <span>Войти</span>
+      </router-link>
+
+      <router-link 
+        v-else 
+        to="/profile" 
+      >
+        <picture>
+          <source
+            type="image/webp"
+            srcset="@/assets/img/users/user5.webp 1x, @/assets/img/users/user5@2x.webp 2x"
+          />
+          <img
+            src="@/assets/img/users/user5.jpg"
+            srcset="@/assets/img/users/user5@2x.jpg"
+            alt="Василий Ложкин"
+            width="32"
+            height="32"
+          />
+        </picture>
+        <span>USERNAME</span>
+      </router-link>
+      <a v-if="isAuthorized" href="/" class="header__logout"><span>Выйти</span></a>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppLayoutHeader",
+  data() {
+    return {
+      isAuthorized: false,
+      //пока для тестирования работы роутинга храним состояние авторизации в компоненте
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.dummy-selector {
+  border: 1px;
+}
+</style>
---
+++ src/frontend/src/layouts/AppLayoutMain.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <AppLayoutHeader />
+    <slot />
+  </div>
+</template>
+
+<script>
+import AppLayoutHeader from "@/layouts/AppLayoutHeader";
+export default {
+  name: "AppLayoutMain",
+  components: {
+    AppLayoutHeader,
+  },
+};
+</script>
+
+<style></style>
